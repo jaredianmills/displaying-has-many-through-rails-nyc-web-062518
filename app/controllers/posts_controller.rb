89afc4e03c942e04a72f17e5178fcf0246676fc0
jaredianmills <jaredianmills@gmail.com>
2018-07-25 16:11:19 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit]
+
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).include(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
   end
 end
